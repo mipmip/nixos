5a58bcf7ad04e3780180adf541168f6b140ccbcd
Pim Snel <pim@lingewoud.nl>
2021-10-06 16:24:42 +0200

    remove source junk

--- pkgs/crelease/source/spec/crelease_spec.cr
+++
@@ -1,9 +0,0 @@
-require "./spec_helper"
-
-describe Bool do
-  # TODO: Write tests
-
-  it "works" do
-    false.should_not eq(true)
-  end
-end
--- pkgs/crelease/source/src/crelease/version.cr
+++
@@ -1,3 +0,0 @@
-module Crelease
-  VERSION = "1.0.0"
-end
