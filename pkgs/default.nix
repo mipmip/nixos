3a55ddc708545950c9c01c2d9a17f8cabbbdfa7e
Pim Snel <pim@lingewoud.nl>
2021-10-06 13:16:52 +0200

    some stuff

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,6 @@ let
   self = rec {
     callPackage_i686 = pkgs.lib.callPackageWith (pkgs.pkgsi686Linux // self);
     smug = pkgs.callPackage ./smug { };
-    #hl4150cdn = pkgs.callPackage ./hl4150cdn { };
     hl4150cdn = callPackage_i686 ./hl4150cdn { };
   };
 in self
