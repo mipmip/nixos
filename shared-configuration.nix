ec268b60941b5fa3f00b8458992dffd9939a88e8
Pim Snel <post@pimsnel.com>
2022-02-02 18:18:59 +0100

    changes from ojs on Wed Feb 2 06:18:59 PM CET 2022

--- shared-configuration.nix
+++ shared-configuration.nix
@@ -5,7 +5,7 @@
   nixpkgs.config.packageOverrides = pkgs: {
 
     unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
-    nixos05 = import <nixos-unstable> { config = { allowUnfree = true; }; };
+    nixos05 = import <nixos05> { config = { allowUnfree = true; }; };
 
     mipmip_pkg = import (./pkgs) {
       inherit pkgs;
