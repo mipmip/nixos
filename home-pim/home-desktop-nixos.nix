55db849f93456dd76ca3604d9f6e106e5aadfe31
Pim Snel <post@pimsnel.com>
2022-02-18 14:21:36 +0100

    changes from ojs on Fri Feb 18 02:21:36 PM CET 2022

--- home-pim/home-desktop-nixos.nix
+++ home-pim/home-desktop-nixos.nix
@@ -30,25 +30,5 @@
     /home/pim/nixos/home-pim/programs
   ];
 
-  xdg.desktopEntries = {
-    st = {
-
-      name           = "st";
-      comment        = "st suckless terminal";
-      genericName    = "st terminal";
-      exec           = "st";
-      categories     = [
-        "TerminalEmulator"
-      ];
-      terminal       = false;
-      startupNotify  = true;
-      type           = "Application";
-      icon           = "org.gnome.Terminal";
-      settings       = {
-        Keywords = "shell;prompt;command;commandline;cmd;";
-        StartupWMClass = "st-256color";
-      };
-    };
-  };
 
 }
---
+++ home-pim/programs/xdg.nix
@@ -0,0 +1,21 @@
+  xdg.desktopEntries = {
+    st = {
+
+      name           = "st";
+      comment        = "st suckless terminal";
+      genericName    = "st terminal";
+      exec           = "st";
+      categories     = [
+        "TerminalEmulator"
+      ];
+      terminal       = false;
+      startupNotify  = true;
+      type           = "Application";
+      icon           = "org.gnome.Terminal";
+      settings       = {
+        Keywords = "shell;prompt;command;commandline;cmd;";
+        StartupWMClass = "st-256color";
+      };
+    };
+  };
+
