31f520f54cce124e88689242d24491dad20b4850
Pim Snel <post@pimsnel.com>
2022-03-04 08:56:58 +0100

    changes from ojs on Fri Mar 4 08:56:58 AM CET 2022

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -2,6 +2,10 @@
 
 {
 
+  environment.sessionVariables = {
+      MOZ_ENABLE_WAYLAND = "1"
+    };
+
   environment.systemPackages = with pkgs; [
     (vim_configurable.override { python = python3; })
   ];
