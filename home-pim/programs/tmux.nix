57009bfac7eb5c3ed766b34e00966fe70d082106
Pim Snel <post@pimsnel.com>
2022-03-09 14:05:52 +0100

    changes from ojs on Wed Mar 9 02:05:52 PM CET 2022

--- home-pim/programs/tmux.nix
+++ home-pim/programs/tmux.nix
@@ -41,7 +41,6 @@
       bind-key -T copy-mode-vi V send -X select-line
       bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
 
-      #unbind l
       unbind C-a
       bind Tab last-window        # move to last active window
 
