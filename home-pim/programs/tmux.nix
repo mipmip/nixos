24bda22638415df0cd749bf295aca8ec16601e62
Pim Snel <post@pimsnel.com>
2022-03-09 10:33:19 +0100

    changes from ojs on Wed Mar 9 10:33:19 AM CET 2022

--- home-pim/programs/tmux.nix
+++ home-pim/programs/tmux.nix
@@ -17,6 +17,30 @@
 
       set -s focus-events on
 
+      set -g base-index 1           # start windows numbering at 1
+      setw -g pane-base-index 1     # make pane numbering consistent with windows
+
+      setw -g automatic-rename off  # rename window to reflect current program
+      set -g renumber-windows on    # renumber windows when a window is closed
+
+      set -g set-titles on          # set terminal title
+      set-option -g set-titles-string '#S'
+
+      set -g display-panes-time 800 # slightly longer pane indicators display time
+      set -g display-time 1000      # slightly longer status messages display time
+
+      set -g status-interval 300     # redraw status line in seconds
+
+      # activity
+      set -g monitor-activity off
+      set -g visual-activity off
+
+# move status line to top
+#set -g status-position top
+      set-option -g default-shell "env zsh"
+
+
+
 
       if '[ -f ~/.tmux/gpakosz.cf ]' 'source ~/.tmux/gpakosz.cf'
       run 'cat ~/.tmux/gpakosz.sh | sh -s _apply_configuration'
