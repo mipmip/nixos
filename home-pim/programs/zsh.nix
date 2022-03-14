6a920a05db3d3aaa52e5b5f31f11dd9051957845
Pim Snel <post@pimsnel.com>
2022-03-14 13:44:22 +0100

    changes from ojs on Mon Mar 14 01:44:22 PM CET 2022

--- home-pim/programs/zsh.nix
+++ home-pim/programs/zsh.nix
@@ -15,7 +15,7 @@
 
     shellAliases = {
       open = "xdg-open";
-      t = "tmux a || smug start lobby";
+      t = "tmux a || smug start lobby && smug start doen && smug start sudo && smug start nixos &&";
     };
 
     plugins = [
