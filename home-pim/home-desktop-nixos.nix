8704d6ec50def2158ec429dde087ebb52c5b3b1c
Pim Snel <post@pimsnel.com>
2022-03-14 14:23:38 +0100

    changes from ojs on Mon Mar 14 02:23:38 PM CET 2022

--- home-pim/home-desktop-nixos.nix
+++ home-pim/home-desktop-nixos.nix
@@ -24,12 +24,12 @@
   home.stateVersion = "21.05";
 
   imports = [
-    /home/pim/nixos/home-pim/programs/vim.nix
     /home/pim/nixos/home-pim/programs/git.nix
     /home/pim/nixos/home-pim/programs/tmux.nix
     /home/pim/nixos/home-pim/programs/fzf.nix
-    /home/pim/nixos/home-pim/programs/zsh.nix
+    /home/pim/nixos/home-pim/programs/vim.nix
     /home/pim/nixos/home-pim/programs/xdg.nix
+    /home/pim/nixos/home-pim/programs/zsh.nix
 
     /home/pim/nixos/home-pim/files
     /home/pim/nixos/home-pim/dconf
