a971f9316c9576e254ef5b55081c1bfb20cce894
Pim Snel <post@pimsnel.com>
2022-03-28 12:54:31 +0200

    changes from ojs on Mon Mar 28 12:54:31 PM CEST 2022

--- home-pim/programs/xdg.nix
+++ home-pim/programs/xdg.nix
@@ -19,5 +19,27 @@
         StartupWMClass = "st-256color";
       };
     };
+
+
+    firefox = {
+
+      name           = "firefox";
+      comment        = "st suckless terminal";
+      genericName    = "st terminal";
+      exec           = "st";
+      categories     = [
+        "TerminalEmulator"
+      ];
+      terminal       = false;
+      startupNotify  = true;
+      type           = "Application";
+      icon           = "org.gnome.Terminal";
+      settings       = {
+        Keywords = "shell;prompt;command;commandline;cmd;";
+        StartupWMClass = "st-256color";
+      };
+    };
+  };
+
   };
 }
