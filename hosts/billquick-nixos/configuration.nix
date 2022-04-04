2e1a6d08bc208a04c90673c8900b59ee615c70b4
Pim Snel <post@pimsnel.com>
2022-04-04 12:04:37 +0200

    changes from billquick on Mon Apr 4 12:04:37 PM CEST 2022

--- hosts/billquick-nixos/configuration.nix
+++ hosts/billquick-nixos/configuration.nix
@@ -50,6 +50,7 @@ in
   networking.useDHCP = false;
   networking.interfaces.wlp0s20u2.useDHCP = true;
 
+networking.hosts =
 {
   "127.0.0.1" = [ "foo.bar.baz" ];
   "192.168.0.2" = [ "fileserver.local" "nameserver.local" ];
