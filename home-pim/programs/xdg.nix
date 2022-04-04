7999d411d3de85d3e7b267d6dd303f6e81ca9ca7
Pim Snel <post@pimsnel.com>
2022-04-04 17:09:19 +0200

    changes from billquick on Mon Apr 4 05:09:19 PM CEST 2022

--- home-pim/programs/xdg.nix
+++ home-pim/programs/xdg.nix
@@ -1,6 +1,27 @@
 { config, pkgs, ... }:
 {
   xdg.desktopEntries = {
+
+    st = {
+
+      name           = "st";
+      comment        = "st suckless terminal";
+      genericName    = "st terminal";
+      exec           = "st";
+      categories     = [
+        "TerminalEmulator"
+      ];
+      terminal       = false;
+      startupNotify  = true;
+      type           = "Application";
+      icon           = "org.gnome.Terminal";
+      settings       = {
+        Keywords = "shell;prompt;command;commandline;cmd;";
+        StartupWMClass = "st-256color";
+      };
+    };
+
+
     st = {
 
       name           = "st";
