b020591ab68cf04558337a87430bea4aa5daab6c
Pim Snel <post@pimsnel.com>
2022-04-26 15:24:04 +0200

    changes from ojs on Tue Apr 26 03:24:04 PM CEST 2022

---
+++ home-pim/programs/xdg.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+{
+  xdg.desktopEntries = {
+
+
+    st = {
+
+      name           = "st";
+      comment        = "st suckless terminal";
+      genericName    = "st terminal";
+      exec           = "st";
+      categories     = [
+        "TerminalEmulator"
+      ];
+      terminal       = false;
+      startupNotify  = true;
+      type           = "Application";
+      icon           = "org.gnome.Terminal";
+      settings       = {
+        Keywords = "shell;prompt;command;commandline;cmd;";
+        StartupWMClass = "st-256color";
+      };
+    };
+
+    vim = {
+
+      name           = "VimWrapped";
+      comment        = "Vim Wrapped";
+      genericName    = "Vim";
+      exec           = "st -e vim";
+      categories     = [
+        "TerminalEmulator"
+      ];
+      terminal       = false;
+      startupNotify  = true;
+      type           = "Application";
+      icon           = "org.gnome.Terminal";
+      settings       = {
+        Keywords = "shell;prompt;command;commandline;cmd;";
+        StartupWMClass = "st-256color";
+      };
+    };
+
+    quiqr-desktop = {
+
+      name           = "Quiqr Desktop";
+      comment        = "Quiqr Desktop";
+      genericName    = "quiqr desktop";
+      exec           = "st";
+      categories     = [ "TerminalEmulator" ];
+      terminal       = false;
+      startupNotify  = true;
+      type           = "Application";
+      icon           = "org.quiqr.quiqr-desktop";
+      settings       = {
+        Keywords = "shell;prompt;command;commandline;cmd;";
+        StartupWMClass = "quiqr";
+      };
+    };
+  };
+}
