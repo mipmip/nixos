9d765075a4829c8ccd1fa5afb9bdcadb781a0770
Pim Snel <post@pimsnel.com>
2022-04-26 15:00:31 +0200

    changes from ojs on Tue Apr 26 03:00:31 PM CEST 2022

--- home-pim/programs/neovim.nix
+++ home-pim/programs/neovim.nix
@@ -17,7 +17,8 @@ colorscheme solarized
 set termguicolors
 let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
 let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-set background=light
+      set background=light
+
     '';
   };
 }
