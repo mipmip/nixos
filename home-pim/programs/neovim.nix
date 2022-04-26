8ff8acfe0ba6218d17db7ef4140c32739798cf98
Pim Snel <post@pimsnel.com>
2022-04-26 14:47:49 +0200

    changes from ojs on Tue Apr 26 02:47:49 PM CEST 2022

--- home-pim/programs/neovim.nix
+++ home-pim/programs/neovim.nix
@@ -6,6 +6,10 @@
     plugins = with pkgs.vimPlugins; [
       vim-javascript
       vim-jsx-pretty
+      lifepillar/vim-solarized8
+      logico/typewriter-vim
+      mipmip/vim-whitewriter', { 'branch': 'main' }
+
     ];
 
     extraConfig = ''
