2552f235fdbd2675b4086a184562f232f8bae0a2
Pim Snel <post@pimsnel.com>
2022-05-05 10:48:57 +0200

    changes from ojs on Thu May 5 10:48:57 AM CEST 2022

---
+++ shells/python/shell.nix
@@ -0,0 +1,14 @@
+# python.nix
+with (import <nixpkgs> {});
+let
+  my-python-packages = python-packages: with python-packages; [
+    # other python packages you want
+  ];
+  python-with-my-packages = python3.withPackages my-python-packages;
+in
+mkShell {
+  buildInputs = [
+    python-with-my-packages
+  ];
+}
+
