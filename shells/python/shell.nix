92f119d9e7d7a9fbe547249ccb72c24fc18b3580
Pim Snel <post@pimsnel.com>
2022-05-05 10:43:29 +0200

    changes from ojs on Thu May 5 10:43:29 AM CEST 2022

---
+++ shells/python/shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.python3Packages.buildPythonApplication {
+  pname = "myapp";
+  src = ./.;
+  version = "0.1";
+  propagatedBuildInputs = [ pkgs.python3Packages.flask ];
+}
+
+
