2954ee78f6625288fd9c7c16caf6eac795f9158d
Pim Snel <post@pimsnel.com>
2022-05-05 10:45:24 +0200

    changes from ojs on Thu May 5 10:45:24 AM CEST 2022

--- shells/python/shell.nix
+++ shells/python/shell.nix
@@ -1,10 +1,42 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.python3Packages.buildPythonApplication {
-  pname = "myapp";
-  src = ./.;
-  version = "0.1";
-  propagatedBuildInputs = [ pkgs.python3Packages.flask ];
+let
+
+    mach-nix = import (
+        builtins.fetchGit {
+            url = "https://github.com/DavHau/mach-nix/";
+            ref = "refs/tags/3.3.0";
+        }
+    ) {};
+
+    custom-python = mach-nix.mkPython {
+        python = "python38Full";
+        requirements = ''
+            pip
+            ipython
+            griddly
+            matplotlib
+            pygame
+        '';
+        providers = {
+            _default = "wheel,sdist,nixpkgs";
+        };
+    };
+in
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    custom-python
+    # tried with and without this next line:
+    gccStdenv
+    # griddly requires the Vulkan-SDK. Is this it?
+    vulkan-headers
+    vulkan-loader
+    vulkan-tools
+  ];
+
+  # tried with and without this next line:
+  LD_LIBRARY_PATH="${lib.makeLibraryPath [stdenv.cc.cc]}";
 }
-
-
