ce75880e355a4afe6f7851feabeed811535f30f5
Pim Snel <post@pimsnel.com>
2022-05-30 21:30:00 +0200

    changes from ojs on Mon May 30 09:30:00 PM CEST 2022

--- pkgs/embgit/default.nix
+++ pkgs/embgit/default.nix
@@ -9,7 +9,7 @@ buildGoModule rec {
     #sha256 = "sha256:04i1ijch1crmgx49nnl1rbly15gwwwm3hic22v1hgsf0d3zhm0sn";
   };
 
-  #vendorSha256 = "sha256:1298s8hyrw7v09hyc4ddkwxdwf0k294n95kv65467y0niw98j6sg";
+  vendorSha256 = "sha256:1298s8hyrw7v09hyc4ddkwxdwf0k294n95kv65467y0niw98j6sg";
 
   meta = with lib; {
     description = ''
