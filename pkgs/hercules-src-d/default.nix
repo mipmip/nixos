2f4a52e305ffc72189073207a9b487774c2c45c8
Pim Snel <post@pimsnel.com>
2022-05-31 14:12:08 +0200

    changes from ojs on Tue May 31 02:12:08 PM CEST 2022

--- pkgs/hercules-src-d/default.nix
+++ pkgs/hercules-src-d/default.nix
@@ -6,10 +6,13 @@ buildGoModule rec {
   src = fetchgit {
     url = "https://github.com/src-d/hercules.git";
     rev = "v${version}";
-    sha256 = "";
+    sha256 = "sha256:1x2v4mv17m3937qhdar38yg43ay725rxvgcqpynh664x1hd3q2rd";
   };
 
-  vendorSha256 = "";
+  vendorSha256 = "sha256:1rba5rpvlr8dyhj145b5i57pm4skfpj3vm7vydkn79k6ak6x985x";
+  };
+
+
 
   meta = with lib; {
     description = ''
