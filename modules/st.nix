75e74ddac5e671e8cf9c4630fa25bc0b920b9f5f
Pim Snel <post@pimsnel.com>
2022-06-28 16:20:56 +0200

    changes from ojs on Tue Jun 28 04:20:56 PM CEST 2022

--- modules/st.nix
+++ modules/st.nix
@@ -7,7 +7,7 @@
       version = "0.8.4";
       src = fetchurl {
         url = "https://dl.suckless.org/st/${pname}-${version}.tar.gz";
-        hash = "19j66fhckihbg30ypngvqc9bcva47mp379ch5vinasjdxgn3qbfl";
+        sha256 = "19j66fhckihbg30ypngvqc9bcva47mp379ch5vinasjdxgn3qbfl";
         };
       buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
       patches = [
