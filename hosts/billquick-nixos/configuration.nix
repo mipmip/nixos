444cb99333ac773c7db0a9ce1b441b7e309d542f
Pim Snel <post@pimsnel.com>
2022-07-26 22:55:18 +0200

    changes from billquick on Tue Jul 26 10:55:18 PM CEST 2022

--- hosts/billquick-nixos/configuration.nix
+++ hosts/billquick-nixos/configuration.nix
@@ -18,6 +18,7 @@ in
 #        ../../modules/dev-crystal.nix
         ../../modules/dev-quiqr.nix
         ../../modules/dev-technative.nix
+        ../../modules/office-communication.nix
         ../../modules/vim-large.nix
         ../../modules/workstation.nix
         ../../modules/workstation-pkg.nix
