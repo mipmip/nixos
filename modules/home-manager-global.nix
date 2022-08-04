a469efb365488b71392e2715631ea1277fa2c439
Pim Snel <post@pimsnel.com>
2022-08-04 20:30:03 +0200

    changes from billquick on Thu Aug 4 08:30:03 PM CEST 2022

--- modules/home-manager-global.nix
+++ modules/home-manager-global.nix
@@ -4,10 +4,6 @@ let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz";
 in
 {
-  environment.systemPackages = with pkgs; [
-    home-manager
-  ];
-
   imports = [
     (import "${home-manager}/nixos")
   ];
