20ee49a60876cb40c612115f84cb7b5189d38d12
Pim Snel <post@pimsnel.com>
2022-09-18 22:23:54 +0200

    changes from ojs on zo 18 sep 2022 22:23:54 CEST

--- pkgs/mip/default.nix
+++ pkgs/mip/default.nix
@@ -28,7 +28,7 @@ crystal.buildCrystalPackage rec {
 
     buildPhase = ''
        mkdir lib2
-       for d in lib/*; do cp -Lr $d lib2/ ; done
+       for d in lib/*; do cp -Lvr $d lib2/ ; done
        mv lib lib3
        mv lib2 lib
 
