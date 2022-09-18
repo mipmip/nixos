a09bedeccd020e1a169b540750d3023a6a859962
Pim Snel <post@pimsnel.com>
2022-09-18 23:19:36 +0200

    changes from ojs on zo 18 sep 2022 23:19:36 CEST

--- pkgs/mip/default.nix
+++ pkgs/mip/default.nix
@@ -49,6 +49,8 @@ crystal.buildCrystalPackage rec {
 
        cd lib/common_marker/ext
        cp -a ${markdfsrc} ./cmark-gfm
+       ls -al
+       ls -al ./cmark-gfm
        sed -i 's/git/echo/g' Makefile
        make
        cd ../../..
