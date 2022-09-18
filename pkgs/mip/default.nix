2a1ed96e6a25efa907a015febc5c634fc3eeec10
Pim Snel <post@pimsnel.com>
2022-09-18 18:48:13 +0200

    changes from ojs on zo 18 sep 2022 18:48:13 CEST

--- pkgs/mip/default.nix
+++ pkgs/mip/default.nix
@@ -8,7 +8,7 @@ crystal.buildCrystalPackage rec {
     owner = "mipmip";
     repo = "mip";
     rev = "8a67307f0b0bf63b005b0e59ec02e85e2516abc9";
-    sha256 = "071xj19jbsz5081v72q37zjxf4ar7ixw1iiawwl461z1ab8ysg48";
+    sha256 = "";
   };
 
   shardsFile = ./shards.nix;
