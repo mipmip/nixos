3677fbb6f0794588aaffe5d4d5348450dc5d7941
Pim Snel <post@pimsnel.com>
2022-09-18 22:14:44 +0200

    changes from ojs on zo 18 sep 2022 22:14:44 CEST

--- pkgs/mip/default.nix
+++ pkgs/mip/default.nix
@@ -26,20 +26,6 @@ crystal.buildCrystalPackage rec {
   shardsFile = ./shards.nix;
   doCheck = false;
 
-  #crystalBinaries.webview.src = "src/lucky.cr";
-  configurePhase = args.configurePhase or lib.concatStringsSep "\n"
-    (
-      [
-        "runHook preConfigure"
-      ]
-      ++ lib.optional (lockFile != null) "cp ${lockFile} ./shard.lock"
-      ++ lib.optionals (shardsFile != null) [
-        "test -e lib || mkdir lib"
-        "for d in ${crystalLib}/*; do cp -av $d lib/; done"
-        "cp shard.lock lib/.shards.info"
-      ]
-      ++ [ "runHook postConfigure" ]
-    );
 
 
     buildPhase = ''
