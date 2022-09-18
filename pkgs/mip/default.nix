dace73d00872f1fd5ee37736aeaf8e3a40f19765
Pim Snel <post@pimsnel.com>
2022-09-18 23:19:10 +0200

    changes from ojs on zo 18 sep 2022 23:19:10 CEST

--- pkgs/mip/default.nix
+++ pkgs/mip/default.nix
@@ -43,7 +43,8 @@ crystal.buildCrystalPackage rec {
        mv lib2 lib
        chmod -R +w lib
 
-       cd lib/webview && make
+       cd lib/webview
+       make
        cd ../..
 
        cd lib/common_marker/ext
