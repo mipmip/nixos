36be229e7f9695c2b632842a0942eaacaf0a3e6a
Pim Snel <post@pimsnel.com>
2022-09-18 22:46:16 +0200

    changes from ojs on zo 18 sep 2022 22:46:16 CEST

--- pkgs/mip/default.nix
+++ pkgs/mip/default.nix
@@ -13,54 +13,6 @@
 }:
 
 crystal.buildCrystalPackage rec {
-  owner = "naqvis";
-  repo = "webview";
-  rev = "v0.1.5";
-  sha256 = "05ay40xl4v8hp23ipgybsyarv5p7ydjrz0zzrfvj7j6gxwnpq89b";
-
-  pname = "webview";
-  rev = "v0.1.3";
-
-  src = fetchFromGitHub {
-    owner = "mipmip";
-    repo = "mip";
-    rev = "0ec44d08ee4dacba063564f6a89bf1b7fcc635b1";
-    sha256 = "sY9gypkDwT4+KGhCvu832G3h4YdGGRpd8eB37Zp1BUo=";
-  };
-
-  doCheck = false;
-
-  nativeBuildInputs = [
-    pkg-config
-    cmake
-    fswatch
-    openssl
-    crystal
-    shards
-    nodejs
-    #yarn
-    webkitgtk
-    cmake
-  ];
-  buildInputs = [
-    fswatch
-    openssl
-    crystal
-    nodejs
-    yarn
-   webkitgtk
-    cmake
-  ];
-
-
-  meta = with lib; {
-    description = "Fast and simple markdown viewer";
-    homepage = "https://github.com/mipmip/mip";
-    license = licenses.mit;
-  };
-}
-
-crystal.buildCrystalPackage rec {
   pname = "mip";
   version = "0.1.3";
 
