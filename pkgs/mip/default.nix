03977f67c6b46813342e7edfb2fe9d4a4f66c2de
Pim Snel <post@pimsnel.com>
2022-09-18 18:43:57 +0200

    changes from ojs on zo 18 sep 2022 18:43:57 CEST

--- pkgs/mip/default.nix
+++ pkgs/mip/default.nix
@@ -7,7 +7,7 @@ crystal.buildCrystalPackage rec {
   src = fetchFromGitHub {
     owner = "mipmip";
     repo = "mip";
-    rev = "846b1c26599ffe90afa96e9f4f64300250aa216d";
+    rev = "8a67307f0b0bf63b005b0e59ec02e85e2516abc9";
     sha256 = "071xj19jbsz5081v72q37zjxf4ar7ixw1iiawwl461z1ab8ysg48";
   };
 
