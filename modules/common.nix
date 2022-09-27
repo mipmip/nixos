ac49a2a89e5f011a1c65c662de34ea29a08f81d1
Pim Snel <post@pimsnel.com>
2022-09-27 15:10:09 +0200

    changes from ojs on di 27 sep 2022 15:10:09 CEST

--- modules/common.nix
+++ modules/common.nix
@@ -9,21 +9,6 @@
   nixpkgs.config = {
 
     allowUnfree = true;
-
-    packageOverrides = super: let self = super.pkgs; in {
-
-      mumble = super.mumble.override { pulseSupport = true; };
-
-      linuxPackages = super.linuxPackages // {
-        e1000e = super.linuxPackages.e1000e.overrideDerivation (old: {
-          name = "e1000e-3.3.3-${config.boot.kernelPackages.kernel.version}";
-          src = fetchurl {
-            url = "https://www.dropbox.com/s/pxx883hx9763ygn/e1000e-3.3.3.tar.gz?dl=0";
-            sha256 = "1s2w54927fsxg0f037h31g3qkajgn5jd0x3yi1chxsyckrcr0x80";
-          };
-        });
-      };
-    };
   }
 
   services.openssh.enable = true;
