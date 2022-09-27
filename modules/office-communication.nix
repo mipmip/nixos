71d6930b2df7bf0ab38f458bdbb033a583d62a69
Pim Snel <post@pimsnel.com>
2022-09-27 15:29:56 +0200

    changes from ojs on di 27 sep 2022 15:29:56 CEST

--- modules/office-communication.nix
+++ modules/office-communication.nix
@@ -4,17 +4,6 @@
 
   environment.systemPackages = with pkgs; [
     (pkgs.slack.overrideAttrs (oldAttrs: rec {
-      desktopItem = oldAttrs.desktopItem.overrideAttrs (desktopAttrs: {
-        buildCommand =
-          let
-            oldExec = builtins.match ".*(\nExec=[^\n]+\n).*" desktopAttrs.buildCommand;
-            matches = oldExec;
-            replacements = [ "\nExec=/usr/bin/slack --enable-features=WebRTCPipeWireCapturer %U\n" ];
-          in
-          assert oldExec != null && oldTerminal != null;
-          builtins.replaceStrings matches replacements desktopAttrs.buildCommand;
-        });
-        postInstall = builtins.replaceStrings [ "${oldAttrs.desktopItem}" ] [ "${desktopItem}" ] oldAttrs.postInstall;
       }))
 
 
