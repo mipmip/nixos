1a9f2bff8e0d9b41f2f4331656d6c6c7bd0d20e7
Pim Snel <post@pimsnel.com>
2022-09-27 15:30:10 +0200

    changes from ojs on di 27 sep 2022 15:30:09 CEST

--- modules/office-communication.nix
+++ modules/office-communication.nix
@@ -4,6 +4,12 @@
 
   environment.systemPackages = with pkgs; [
     (pkgs.slack.overrideAttrs (oldAttrs: rec {
+
+
+        installPhase = old.installPhase + ''
+            echo "${(import ./companion-cube.cow)}" > $out/share/cows/companion-cube.cow
+              '';
+
       }))
 
 
