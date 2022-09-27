b69f20ba705001a04a3640126ec83e164c61e86c
Pim Snel <post@pimsnel.com>
2022-09-27 15:32:24 +0200

    changes from ojs on di 27 sep 2022 15:32:24 CEST

--- modules/office-communication.nix
+++ modules/office-communication.nix
@@ -15,15 +15,15 @@
         # Otherwise it looks "suspicious"
         chmod -R g-w $out
         for file in $(find $out -type f \( -perm /0111 -o -name \*.so\* \) ); do
-        patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
-        patchelf --set-rpath ${rpath}:$out/lib/slack $file || true
+          patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
+          patchelf --set-rpath ${rpath}:$out/lib/slack $file || true
         done
         # Replace the broken bin/slack symlink with a startup wrapper
         rm $out/bin/slack
         makeWrapper $out/lib/slack/slack $out/bin/slack \
-        --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
-        --prefix PATH : ${lib.makeBinPath [xdg-utils]} \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--enable-features=UseOzonePlatform --ozone-platform=wayland}}"
+          --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+          --prefix PATH : ${lib.makeBinPath [xdg-utils]} \
+          --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--enable-features=UseOzonePlatform --ozone-platform=wayland}}"
         # Fix the desktop link
         substituteInPlace $out/share/applications/slack.desktop \
           --replace /usr/bin/ $out/bin/ \
