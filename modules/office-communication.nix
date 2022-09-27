216eef4a99f770ac78c96943d8b7bc8a3f67e598
Pim Snel <post@pimsnel.com>
2022-09-27 15:13:52 +0200

    changes from ojs on di 27 sep 2022 15:13:52 CEST

--- modules/office-communication.nix
+++ modules/office-communication.nix
@@ -7,9 +7,8 @@
       buildCommand =
         let
           oldExec = builtins.match ".*(\nExec=[^\n]+\n).*" desktopAttrs.buildCommand;
-          oldTerminal = builtins.match ".*(\nTerminal=[^\n]+\n).*" desktopAttrs.buildCommand;
           matches = oldExec ++ oldTerminal;
-          replacements = [ "\nExec=/my_exec\n" "\nTerminal=true\n" ];
+          replacements = [ "\nExec=/my_exec\n" ];
         in
         assert oldExec != null && oldTerminal != null;
         builtins.replaceStrings matches replacements desktopAttrs.buildCommand;
