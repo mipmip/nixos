d5f6b348b3bb1d65bb98fbbc7fce88d183ce7788
Pim Snel <post@pimsnel.com>
2022-09-27 15:22:00 +0200

    changes from ojs on di 27 sep 2022 15:22:00 CEST

--- modules/office-communication.nix
+++ modules/office-communication.nix
@@ -2,25 +2,23 @@
 
 {
 
-
-
   environment.systemPackages = with pkgs; [
-      (pkgs.slack.overrideAttrs (oldAttrs: rec {
-    desktopItem = oldAttrs.desktopItem.overrideAttrs (desktopAttrs: {
-      buildCommand =
-        let
-          oldExec = builtins.match ".*(\nExec=[^\n]+\n).*" desktopAttrs.buildCommand;
-          matches = oldExec;
-          replacements = [ "\nExec=/usr/bin/slack --enable-features=WebRTCPipeWireCapturer %U\n" ];
-        in
-        assert oldExec != null && oldTerminal != null;
-        builtins.replaceStrings matches replacements desktopAttrs.buildCommand;
-      });
-      postInstall = builtins.replaceStrings [ "${oldAttrs.desktopItem}" ] [ "${desktopItem}" ] oldAttrs.postInstall;
-    }))
+    (pkgs.slack.overrideAttrs (oldAttrs: rec {
+      desktopItem = oldAttrs.desktopItem.overrideAttrs (desktopAttrs: {
+        buildCommand =
+          let
+            oldExec = builtins.match ".*(\nExec=[^\n]+\n).*" desktopAttrs.buildCommand;
+            matches = oldExec;
+            replacements = [ "\nExec=/usr/bin/slack --enable-features=WebRTCPipeWireCapturer %U\n" ];
+          in
+          assert oldExec != null && oldTerminal != null;
+          builtins.replaceStrings matches replacements desktopAttrs.buildCommand;
+        });
+        postInstall = builtins.replaceStrings [ "${oldAttrs.desktopItem}" ] [ "${desktopItem}" ] oldAttrs.postInstall;
+      }))
 
 
-    zoom-us
-    teams
-  ];
-}
+      zoom-us
+      teams
+    ];
+  }
