381a9260cd56db1684692af7e244274070c38377
Pim Snel <post@pimsnel.com>
2022-10-12 16:28:58 +0200

    changes from ojs on Wed Oct 12 04:28:57 PM CEST 2022

--- home-pim/programs/xdg.nix
+++ home-pim/programs/xdg.nix
@@ -15,7 +15,7 @@
       terminal       = false;
       startupNotify  = true;
       type           = "Application";
-      icon           = "org.gnome.Terminal";
+      icon           = "org.gnome.Console";
       settings       = {
         Keywords = "shell;prompt;command;commandline;cmd;";
         StartupWMClass = "st-256color";
