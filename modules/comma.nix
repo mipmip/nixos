2a77c46b7f3c9848e1c6377064c97d775c5a06de
Pim Snel <post@pimsnel.com>
2022-10-14 17:30:21 +0200

    changes from ojs on Fri Oct 14 05:30:21 PM CEST 2022

--- modules/comma.nix
+++ modules/comma.nix
@@ -1,11 +1,13 @@
 { config, lib, pkgs, ... }:
 
-environment.systemPackages =
-let
-  comma = (import (pkgs.fetchFromGitHub {
-    owner = "nix-community";
-    repo = "comma";
-    rev = "v1.2.0";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
-  })).default;
-in [ comma ];
+{
+  environment.systemPackages =
+    let
+      comma = (import (pkgs.fetchFromGitHub {
+        owner = "nix-community";
+        repo = "comma";
+        rev = "v1.3.0";
+        sha256 = "rXAX14yB8v9BOG4ZsdGEedpZAnNqhQ4DtjQwzFX/TLY=";
+      })).default;
+    in [ comma ];
+}
