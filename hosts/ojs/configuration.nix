74660132dfd942559954f849985d2af986264f4f
Pim Snel <post@pimsnel.com>
2022-10-31 19:23:13 +0100

    changes from ojs on ma 31 okt 2022 19:23:12 CET

--- hosts/ojs/configuration.nix
+++ hosts/ojs/configuration.nix
@@ -73,8 +73,7 @@ in
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
       # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.100.0.2/24" ];
-#      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+      ips = [ "10.0.0.126/32" ];
 
       # Path to the private key file.
       #
