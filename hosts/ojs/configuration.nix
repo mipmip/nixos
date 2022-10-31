0e04653cd907fb109c3488f54f3f17b23ba2a617
Pim Snel <post@pimsnel.com>
2022-10-31 19:22:24 +0100

    changes from ojs on ma 31 okt 2022 19:22:24 CET

--- hosts/ojs/configuration.nix
+++ hosts/ojs/configuration.nix
@@ -72,13 +72,14 @@ in
 #  networking.firewall = {
 #    allowedUDPPorts = [ 51820 ]; # Clients and peers can use the same port, see listenport
 #  };
+
   # Enable WireGuard
   networking.wireguard.interfaces = {
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
       # Determines the IP address and subnet of the client's end of the tunnel interface.
       ips = [ "10.100.0.2/24" ];
-      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+#      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
 
       # Path to the private key file.
       #
