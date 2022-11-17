34bb58052ba88c0931548da319f68565d28aafcf
Pim Snel <post@pimsnel.com>
2022-11-17 11:57:24 +0100

    multiple additions for ojs and adevinta

--- modules/dev-technative.nix
+++ modules/dev-technative.nix
@@ -1,8 +1,28 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, unstable, ... }:
 
 {
 
   services.onedrive.enable = true;
+
+  services.globalprotect = {
+    enable = true;
+    #csdWrapper = "${pkgs.openconnect}/libexec/openconnect/hipreport.sh";
+    csdWrapper = "/etc/secrets/hipreport-adevinta.sh";
+#    settings = {
+#      vpn.advgo.net = {
+#        openconnect-args="--csd-wrapper=/etc/secrets/hipreport-adevinta.sh --dump-http-traffic --useragent='PAN GlobalProtect/5.2.11-10 (Apple Mac OS X 12.4.0)' --version-string='5.2.11-10' --os=mac-intel";
+#      };
+#    };
+  };
+
+  programs.proxychains.enable = true;
+  programs.proxychains.proxies = {
+    myproxy = {
+      type = "socks4";
+      host = "127.0.0.1";
+      port = 1080;
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     pre-commit
@@ -21,17 +41,24 @@
     pass
     pinentry-gtk2
     graphviz
+
     #unstable.terracognita
-    #RANDSTAD
-#    citrix_workspace
+
     sq
     terraform-docs
     terrascan
     tflint
 
+    #RANDSTAD
+    #citrix_workspace
+
     #TRACKLIB
     wireguard-tools
 
+    #ADEVINTA
+    unstable.globalprotect-openconnect
+    openconnect
+    proxychains
 
   ];
 }
