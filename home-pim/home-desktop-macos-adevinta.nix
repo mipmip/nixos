8de361f0c5f3a025f87097a93490f665db42dd84
Pim Snel <post@pimsnel.com>
2022-12-12 09:48:06 +0100

    fixes adevinta

--- home-pim/home-desktop-macos-adevinta.nix
+++ home-pim/home-desktop-macos-adevinta.nix
@@ -2,14 +2,15 @@
 
 {
   programs.home-manager.enable = true;
-  home.username = "pim";
-  home.homeDirectory = "/Users/pim";
+  home.username = "pim.snel";
+  home.homeDirectory = "/Users/pim.snel";
   home.stateVersion = "22.05";
 
   imports = [
     /Users/pim.snel/nixos/home-pim/files-main
     /Users/pim.snel/nixos/home-pim/files-macos
     /Users/pim.snel/nixos/home-pim/files-secondbrain
+    /Users/pim.snel/nixos/home-pim/files-i-am-desktop
     /Users/pim.snel/nixos/home-pim/programs/fzf.nix
     /Users/pim.snel/nixos/home-pim/programs/git.nix
     /Users/pim.snel/nixos/home-pim/programs/tmux.nix
