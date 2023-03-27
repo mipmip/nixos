059576c2d6f1f8e9b84eae57275834dc18cdf412
Pim Snel <post@pimsnel.com>
2023-03-27 18:03:55 +0200

    add repo install tool skull

---
+++ pkgs/pims-projects/skull/default.nix
@@ -0,0 +1,22 @@
+{ lib, fetchFromGitHub, crystal }:
+
+crystal.buildCrystalPackage rec {
+  pname = "skull";
+  version = "v0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "mipmip";
+    repo = "skull";
+    rev = "v0.1.1";
+    sha256 = "sha256-/D0i69OlyM5PcXjV/D2fi60wZAylVyWxPKdJ8xd5uPg=";
+  };
+
+  shardsFile = ./shards.nix;
+  doCheck = false;
+
+  meta = with lib; {
+    description = "git repos organized by install tool";
+    homepage = "https://github.com/mipmip/skull";
+    license = licenses.mit;
+  };
+}
