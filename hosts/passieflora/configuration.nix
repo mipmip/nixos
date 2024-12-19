8be450c537f5ac568bfb3a42718fb448f79cd6ae
Pim Snel <post@pimsnel.com>
2024-12-19 21:59:18 +0100

    try apple fix

--- hosts/passieflora/configuration.nix
+++ hosts/passieflora/configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./kernel-and-boot.nix
     ];
 
   boot.loader.systemd-boot.enable = true;
@@ -13,13 +14,8 @@
   boot.plymouth.enable = true;
   boot.plymouth.theme = "breeze";
 
-
-
-
   networking.useDHCP = lib.mkDefault true;
   networking.hostName = "passieflora"; # Define your hostname.
-
-
 
   time.timeZone = "Europe/Amsterdam";
 
@@ -30,7 +26,9 @@
   users.users.pim = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    shell = pkgs.zsh
   };
+  programs.zsh.enable = true;
 
   environment.systemPackages = with pkgs; [
     vim
