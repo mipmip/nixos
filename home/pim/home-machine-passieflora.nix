ecdaeb546f2a9455deff1f5c99290a2c585231cd
Pim Snel <post@pimsnel.com>
2024-12-26 15:16:08 +0100

    big refactor WIP

--- home/pim/home-machine-passieflora.nix
+++ home/pim/home-machine-passieflora.nix
@@ -1,8 +1,5 @@
 { lib, config, pkgs, ... }:
 
-let
-  mkTuple = lib.hm.gvariant.mkTuple;
-in
 {
   imports = [
     ./_hm-modules
@@ -14,38 +11,4 @@ in
 
   dotfiles.awsstuff.enable = false;
   services.secondbrain.enable = true;
-
-  #  programs.myhotkeys."Gnome Extra".cyclewindow.key = "<ALT>grave";
-  #  programs.myhotkeys."Gnome Extra".cyclewindow.description = "Cycle windows within same Application";
-  #  programs.myhotkeys.enable = true;
-  #  programs.myhotkeys.hotkey_groups = [
-  #    {
-  #      name = "Gnome Extra";
-  #      shortcuts = [
-  #        {
-  #          key = "<ALT>grave";
-  #          description = "Cycle windows within same Application";
-  #        }
-  #      ];
-  #    }
-  #
-  #  ];
-  #
-
-#  dconf.settings = {
-#    "org/gnome/desktop/input-sources" = {
-#      per-window = false;
-#      sources = [ (mkTuple [ "xkb" "us" ]) ];
-#      xkb-options = [
-#        "altwin:swap_alt_win"
-#
-#        "grp:alt_shift_toggle"
-#        "lv3:ralt_switch"
-#        "compose:ralt"
-#        "caps:none"
-#      ];
-#    };
-#  };
-
-
 }
