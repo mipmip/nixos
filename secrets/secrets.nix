6cdc4d4e82f06b0f28a5ebf6ac1535f5826b1b37
Pim Snel <post@pimsnel.com>
2024-12-30 13:17:07 +0100

    add hurry server to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,15 @@
 let
   pim = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEY25ZaYRuKUJuVuzqK4c8dKkSxN6Cd9yhbDTa/5Njmh";
+
   users = [ pim ];
 
   ojs   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINNnavv0c8Htl2OSN9sFM/aFm6FbxvHwTLZDjgb5g1zh";
   lego1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/5cm8VDfCN5y05tcX16tZl3rR+kEgznsrEw1FAaoez";
   rodin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDkMd+l2gSXGsWs4FypOt58GTgGruSHravDHPSW1w8XM";
-  systems = [ ojs lego1 rodin];
+  hurry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyh9gDCDN2rzAExllvavzVVr4XkKea63Wa+B7JO8+Qz";
+
+  systems = [ ojs lego1 rodin hurry ];
+
 in
 {
   "openai-api-key.age".publicKeys = [ pim  ojs lego1 rodin];
@@ -15,4 +19,6 @@ in
   "aws-config-copy-first-time-only.age".publicKeys = [ pim];
 
   "aws-accounts.json.age".publicKeys = [ pim  ojs lego1 rodin];
+
+  "hurry-smtp".publicKeys = [pim hurry];
 }
