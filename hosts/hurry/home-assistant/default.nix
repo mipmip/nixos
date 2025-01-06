858b30d4e0cd9564482b3686c0603f223e4cf1d7
Pim Snel <post@pimsnel.com>
2025-01-06 15:49:44 +0100

    more conf for ha

--- hosts/hurry/home-assistant/default.nix
+++ hosts/hurry/home-assistant/default.nix
@@ -1,27 +1,46 @@
 {config, pkgs, ...}:{
+
+
   services.home-assistant = {
+    # opt-out from declarative configuration management
     enable = true;
+    package = (pkgs.home-assistant.override { extraPackages = ps: [
+      ps.numpy
+      ps.aiohomekit
+      #      #ps.pyhap
+      ps.pyatv
+      ps.aiohue
+      ps.gtts
+      ps.pytado
+      ps.tuyaha
+      ps.python-otbr-api
+      ps.tinytuya
+      ps.tuya-iot-py-sdk
+      ps.tuya-device-sharing-sdk
+      ps.pynacl
+      ps.pytile
+      ps.av
+      ps.hap-python
+      ps.async-upnp-client
+      ps.aiodhcpwatcher
+      ps.go2rtc-client
+      ps.python-matter-server
+      ps.universal-silabs-flasher
+      ps.zha
+      ps.aiodiscover
+      ps.pyturbojpeg
+      ps.base36
+    ]; });
+    config = null;
+    lovelaceConfig = null;
+    # configure the path to your config directory
+    configDir = "/etc/home-assistant";
+    # specify list of components required by your configuration
     extraComponents = [
-      # Components required to complete the onboarding
       "esphome"
       "met"
       "radio_browser"
     ];
-
-    customComponents = with pkgs.home-assistant-custom-components; [
-      prometheus_sensor
-      #"system_health"
-      localtuya
-      #"pushover"
-      #"telegram"
-      #"timer"
-      #"tado"
-      #"systemmonitor"
-    ];
-    config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
-    };
   };
 }
+
