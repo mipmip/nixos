561238680a473e345949ff37df8ce10459e751f6
Pim Snel <post@pimsnel.com>
2025-04-29 16:33:06 +0200

    fix typescript in nvim

--- home/pim/_hm-modules/programs/neovim/nvim/lua/plugins/lsconfmason.lua
+++ home/pim/_hm-modules/programs/neovim/nvim/lua/plugins/lsconfmason.lua
@@ -84,12 +84,12 @@ return {
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
       -- Configure diagnostic icons.
-      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
+--      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+--
+--      for type, icon in pairs(signs) do
+--        local hl = "DiagnosticSign" .. type
+--        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+--      end
 
     end,
   },
