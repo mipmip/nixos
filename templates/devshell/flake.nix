39af07639b9485cb5713848defae662ee888f791
Pim Snel <post@pimsnel.com>
2025-06-24 17:04:04 +0200

    initial working version of devshell tpl

--- templates/devshell/flake.nix
+++ templates/devshell/flake.nix
@@ -3,26 +3,18 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devshell.url = "github:numtide/devshell";
-    bumper.url = "github:VTimofeenko/writing-flake-modules?dir=example-1-configurable-inputs-bumper/provider";
   };
 
   outputs =
-  inputs@{ flake-parts, ... }:
-  flake-parts.lib.mkFlake { inherit inputs; } {
-    imports = [
-      inputs.devshell.flakeModule
-      inputs.bumper.flakeModule
-    ];
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    perSystem = _: {
-      devshells.default = { };
-      bumper.changingInputs = [ "bumper" "devshell"];
-      bumper.bumpAllInputs = true;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+    {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      perSystem = _: {
+        devshells.default = { };
+      };
     };
-  };
 }
