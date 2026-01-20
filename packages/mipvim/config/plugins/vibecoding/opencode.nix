97da7bd3731c0032b50654611303b40bb7b91160
Pim Snel <post@pimsnel.com>
2026-01-20 14:39:22 +0100

    auto run after home-manager switch

--- packages/mipvim/config/plugins/vibecoding/opencode.nix
+++ packages/mipvim/config/plugins/vibecoding/opencode.nix
@@ -2,14 +2,18 @@
 {
   plugins.opencode = {
     enable = true;
+    settings = {
+      auto_reload = true;
+      #port = 9090;
+    };
   };
 
-  keymaps = [
-    {
-      mode = [ "n" ];
-      key = "<leader>aa";
-      action = lib.nixvim.mkRaw ''function() require("opencode").ask() end'';
-      options.desc = "AI: Ask a question";
-    }
-  ];
+  #  keymaps = [
+  #   {
+  #      mode = [ "n" ];
+  #      key = "<leader>aa";
+  #      action = lib.nixvim.mkRaw ''function() require("opencode").ask() end'';
+  #      options.desc = "AI: Ask a question";
+  #    }
+  #  ];
 }
