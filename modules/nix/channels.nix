b8d10e4cc7556909fddd5140e9a676b5a414ef94
Pim Snel <post@pimsnel.com>
2026-02-08 23:20:58 +0100

    auto run after nixos-rebuild switch

--- modules/nix/channels.nix
+++ modules/nix/channels.nix
@@ -20,4 +20,25 @@
 
       ];
     };
+
+  flake.modules.nixos.nix-channels-mama =
+    let
+      initChannel = channel: final:
+        import channel { inherit (final) config system; };
+    in
+      {
+      nixpkgs.overlays = [
+
+        inputs.self.overlays.chipsailing
+
+        (final: _prev: {
+          unstable = initChannel inputs.unstable final;
+
+          # TODO use this the other way around: makeNixos { channel = nixpkgs-mama; }
+          # nixmama = initChannel inputs.nixpkgs-mama final;
+        })
+
+      ];
+    };
+
 }
