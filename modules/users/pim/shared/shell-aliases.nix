1a32f8406b23ed278bce12d74a25b49f2b46346b
Pim Snel <post@pimsnel.com>
2026-02-10 16:29:40 +0100

    auto run after home-manager switch

--- modules/users/pim/shared/shell-aliases.nix
+++ modules/users/pim/shared/shell-aliases.nix
@@ -23,7 +23,7 @@ inputs,
     config = lib.mkMerge [
       {
         shared.shellAliases = {
-          open = "dp xdg-open";
+          open = "xdg-open";
           vim = "nvim";
           signal = ''signal-desktop --password-store="gnome-libsecret"'';
 
